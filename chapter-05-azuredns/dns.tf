dc9b59b3f53a34b49d99f50fb51e4cf3d76175a3
vampire-yuta <y-kujirai@vamdemicsystem.black>
2021-04-04 04:31:49 +0900

    add dns zone nameservers

--- chapter-05-azuredns/dns.tf
+++ chapter-05-azuredns/dns.tf
@@ -3,3 +3,15 @@ resource "azurerm_dns_zone" "example-public" {
   resource_group_name = azurerm_resource_group.resource_group.name
 }
 
+resource "azurerm_dns_a_record" "lb" {
+  name                = "udemy"
+  zone_name           = azurerm_dns_zone.example-public.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  ttl                 = 300
+  target_resource_id  = azurerm_public_ip.example.id
+  # records = ["10.0.2.4"]
+}
+
+output "azure_dns_nameservers" {
+  value = azurerm_dns_zone.example-public.name_servers
+t }
